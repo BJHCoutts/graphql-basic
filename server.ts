3a0cc602c19d03f33101cfa0182d89c394439f11
Brian Coutts <bjhcoutts@gmail.com>
2020-06-11 22:55:56 -0700

    Mutations added (adding entries)

--- server.ts
+++ server.ts
@@ -110,13 +110,50 @@ const rootQueryType = new GraphQLObjectType({
 	})
 })
 
+const rootMutationType = new GraphQLObjectType({
+	name: 'Mutation',
+	description: 'Root Mutation',
+	fields: () => ({
+		addBook:{
+			type: BookType,
+			description: 'Add a book',
+			args: {
+				name: {type: GraphQLNonNull(GraphQLString)},
+				authorId: {type: GraphQLNonNull(GraphQLInt)}
+			},
+			resolve: (parent, args) => {
+				const book = {
+					id: books.length +1,
+					name: args.name,
+					authorId: args.author,
+				}
+				books.push(book)
+				return book
+			}
+		},
+		addAuthor:{
+			type: AuthorType,
+			description: 'Add an author',
+			args: {
+				name: {type: GraphQLNonNull(GraphQLString)}
+			},
+			resolve: (parent, args) => {
+				const author = {id: authors.length +1, name: args.name}
+				authors.push(author)
+				return author
+			}
+		}
+	})
+})
+
 const schema = new GraphQLSchema({
-	query: rootQueryType
+	query: rootQueryType,
+	mutation: rootMutationType
 })
 
 app.use('/graphql', expressGraphQL({
 	graphiql: true,
-	schema: rootQueryType
+	schema
 }))
 
 app.listen(5000, () => console.log('listening'))
